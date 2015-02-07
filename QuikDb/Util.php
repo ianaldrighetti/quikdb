07174a642531a90450c7929031bc90826d315ba3
Ian Aldrighetti <ianaldrighetti@outlook.com>
2015-02-07 15:15:54 -0800

    Just getting started.

---
+++ QuikDb/Util.php
@@ -0,0 +1,16 @@
+<?php
+namespace QuikDb;
+
+class Util
+{
+    /**
+     * Indicates whether the specified name is allowed (starts with a character, then alphanumeric and _ allowed).
+     *
+     * @param $name The name to check.
+     * @return bool
+     */
+    public static function isNameAllowed($name)
+    {
+        return preg_match('~^[a-z][a-z0-9_]*$~i', $name) !== false;
+    }
+}
